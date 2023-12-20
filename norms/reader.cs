0d236125d914b6704a46281007e46d7635bbcc2d
maximalekseenko <max.alekseenko@gmail.com>
2023-12-20 11:40:45 +0300

    created reader module

---
+++ norms/reader.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+
+
+namespace NormControl.Norms
+{
+    public class Reader { 
+        public static Norm? Read(string name) {
+            return null;
+        }
+
+        public static void Write(Norm norm) {
+        }
+
+        
+        public static List<Norm> ReadAll() {
+            return new List<Norm>();
+        }
+    }
+}
