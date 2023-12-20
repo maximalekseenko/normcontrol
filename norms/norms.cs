1d6c581838ca43af6651f20d50e15e7bc302eaaa
maximalekseenko <max.alekseenko@gmail.com>
2023-12-20 11:40:18 +0300

    created core object classes

---
+++ norms/norms.cs
@@ -0,0 +1,8 @@
+
+namespace NormControl.Norms
+{
+    public class Norm { 
+        public string name = "new norm";
+        public Style[] styles = new Style[6];
+    }
+}
---
+++ norms/style.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace NormControl.Norms
+{
+    public class Style { 
+        public string font_family { get; set; } = "arial,sans-serif";
+        public string font_size {get; set; } = "12px";
+        public bool is_bold {get; set; } = false;
+    }
+}
