853696168e74e913de257017d1eb63140ef9fbdd
maximalekseenko <max.alekseenko@gmail.com>
2023-12-21 22:12:19 +0300

    default style fix

--- norms/style.cs
+++ norms/style.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace NormControl.Norms
 {
     public class Style { 
-        public string font_family { get; set; } = "arial,sans-serif";
+        public string font_family { get; set; } = "Arial";
         public int font_size {get; set; }
         public bool is_bold {get; set; } = false;
         public bool is_italic {get; set; } = false;
